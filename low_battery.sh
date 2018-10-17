b75230c6eda8e5e6d55a63a98b9355d30745846d
Karan Suthar <karansthr97@gmail.com>
2018-10-17 15:52:19 +0530

    update low_battery.sh

--- low_battery.sh
+++ low_battery.sh
@@ -1,8 +1,10 @@
 BATPATH=/sys/class/power_supply/BAT1
 CHARGE_NOW=`cat $BATPATH/capacity`
 STATUS=`cat $BATPATH/status`
-TIME_LEFT=`expr 1.5*$CHARGE_NOW | bc` 
-if [ $CHARGE_NOW -lt 15 -a  $STATUS != "Charging" ]
+LOW=15                                          # battery percent after which it will show low battery notification if discharging 
+MINUTES_PER_ONE_PERCENT=1.5             	# this is used for calcuting remaining battery juice in minutes
+TIME_LEFT=`expr $MINUTES_PER_ONE_PERCENT*$CHARGE_NOW | bc` 
+if [ $CHARGE_NOW -lt $LOW -a  $STATUS != "Charging" ]
 then
 	/usr/bin/notify-send --urgency=critical "Low battery !" "Less than `printf "%.0f" $TIME_LEFT` minutes of charge left"
 fi
